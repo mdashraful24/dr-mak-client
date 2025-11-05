a2a9d2168f9bf9990d570bb8fcf9b3a10abe3536
mdashraful24 <mdashrafulislam2882@gmail.com>
2025-11-05 20:10:01 +0600

    added project background color

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import { router } from './router/router.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <div className='min-h-screen bg-linear-to-br from-gray-100 to-blue-50'>
+      <RouterProvider router={router} />
+    </div>
   </StrictMode>
 )
