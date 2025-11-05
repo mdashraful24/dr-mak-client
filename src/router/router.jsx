d9ff52ac33d5e83111ee9d77bb36272fea78804f
mdashraful24 <mdashrafulislam2882@gmail.com>
2025-11-05 19:53:03 +0600

    added auth, dashboard layouts and updated fallback 404

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,17 +2,47 @@ import { createBrowserRouter } from "react-router";
 import PublicLayout from "../layouts/PublicLayout";
 import Home from "../pages/Home/Home";
 import ErrorPage from "../components/common/ErrorPage";
+import DashboardLayout from "../layouts/DashboardLayout";
+import DoctorDashboard from "../pages/Dashboard/DoctorDashboard/DoctorDashboard";
+import AuthLayout from "../layouts/AuthLayout";
+import Login from "../pages/Auth/Login";
+import Register from "../pages/Auth/Register";
 
 export const router = createBrowserRouter([
+    // Public Layout
     {
         path: "/",
         Component: PublicLayout,
+        errorElement: <ErrorPage />,
         children: [
-            { index: true, Component: Home }
-        ]
+            { index: true, Component: Home },
+        ],
     },
+
+    // Dashboard Layout
+    {
+        path: "dashboard",
+        Component: DashboardLayout,
+        errorElement: <ErrorPage />,
+        children: [
+            { index: true, Component: DoctorDashboard },
+        ],
+    },
+
+    // Auth Layout
+    {
+        path: "auth",
+        Component: AuthLayout,
+        errorElement: <ErrorPage />,
+        children: [
+            { path: "login", Component: Login },
+            { path: "register", Component: Register },
+        ],
+    },
+
+    // Fallback 404
     {
         path: "*",
-        Component: ErrorPage
-    }
+        element: <ErrorPage />,
+    },
 ]);
